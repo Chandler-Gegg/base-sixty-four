a60967ffd7630d58b04957459bd87a117fbe7cc3
Chandler Gegg <chandlergegg@gmail.com>
2018-10-30 20:56:17 -0500

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-
+declare var require: any;
+// const domtoimage = require('dom-to-image');
+import domtoimage from 'dom-to-image';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  // imageUrl: string;
+  base64: string;
+  imageUrl = 'https://carboncostume.com/wordpress/wp-content/uploads/2013/07/Homer-Simpson.jpg';
+  constructor() {
+    // this.imageUrl = 'https://carboncostume.com/wordpress/wp-content/uploads/2013/07/Homer-Simpson.jpg';
+    // this.imageUrl = '../assets/Homer-Simpson.jpg';
+    console.log('APP COMPONENT');
+
+  }
+  convertToBase64() {
+    // const image = document.createElement('img');
+    // image.src = this.imageUrl;
+    const imgNode = document.getElementById(`image`);
+    // if (imgNode ) {
+      console.log('SELECTED IMAGE');
+      console.log(imgNode);
+      console.log('SELECTED IMAGE');
+      domtoimage.toPng(imgNode)
+      .then( (dataUrl: string) => {
+        console.log('SELECTED IMAGE 2');
+        console.log(dataUrl);
+        this.base64 = dataUrl;
+        console.log('SELECTED IMAGE 2');
+      }).catch( (e: any) => {
+        console.log('SELECTED IMAGE BASE64 SOMETHING WENT WRONG');
+        console.log(e);
+      });
+    // }
+  }
 }
